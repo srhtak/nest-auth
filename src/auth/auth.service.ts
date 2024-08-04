f5f02f4be9868adff716b366af77b6e40e3ec0f7
srhtak <srhtak777@gmail.com>
2024-08-04 13:18:03 +0300

    feat: Add async/await to user validation in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async validateUser(username: string, password: string): Promise<any> {
-    const user = this.usersService.findOne(username);
+    const user = await this.usersService.findOne(username);
     if (user && user.password === password) {
       const { username, password, ...rest } = user;
       return rest;
